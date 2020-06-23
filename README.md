88a90dc6217fcd86e9d5116aa2c57399a0e361ca
nuno <ngplbc@gmail.com>
2020-06-23 18:47:58 +0100

    populate employees

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
-# Express + MySQL boilerplate
-
 ## Run the app locally (in development):
 ```
 yarn dev
--- mocks/employees.js
+++ mocks/employees.js
@@ -32,4 +32,37 @@ module.exports.employees = [
         updated: "2019-04-26",
         photoUrl: "https://s3.amazonaws.com/uifaces/faces/twitter/olegpogodaev/128.jpg"
     },
-]
+    {
+        id: 4,
+        name: "George Bauch",
+        birthdate: "1990-06-01",
+        address: "Douglas Extension, 7, McKenzie",
+        status: "Inactive",
+        position: "Scrum Master",
+        created: "2017-06-01",
+        updated: "2019-04-26",
+        photoUrl: "https://s3.amazonaws.com/uifaces/faces/twitter/calebogden/128.jpg"
+    },
+    {
+        id: 5,
+        name: "Oleg Yon",
+        birthdate: "1980-06-10",
+        address: "Victor Plains, 15, Wisokyburgh",
+        status: "Active",
+        position: "Recuiter",
+        created: "2017-06-01",
+        updated: "2019-04-26",
+        photoUrl: "https://s3.amazonaws.com/uifaces/faces/twitter/olegpogodaev/128.jpg"
+    },
+    {
+        id: 6,
+        name: "Edward Tod",
+        birthdate: "1999-09-21",
+        address: "Kulas Light, 72, Hoeger",
+        status: "Active",
+        position: "Manager",
+        created: "2017-06-01",
+        updated: "2019-04-26",
+        photoUrl: "https://s3.amazonaws.com/uifaces/faces/twitter/mrmoiree/128.jpg"
+    },
+];
